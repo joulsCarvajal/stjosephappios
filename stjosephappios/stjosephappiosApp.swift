9313e1ad0bc1770cdbae81c5f4ecb16053aecb60
Julian Carvajal <nettoingenieria@gmail.com>
2025-01-03 21:33:12 -0500

    Initial Commit

---
+++ stjosephappios/stjosephappiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  stjosephappiosApp.swift
+//  stjosephappios
+//
+//  Created by Julian Carvajal on 3/01/25.
+//
+
+import SwiftUI
+
+@main
+struct stjosephappiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ stjosephappiosTests/stjosephappiosTests.swift
@@ -0,0 +1,17 @@
+//
+//  stjosephappiosTests.swift
+//  stjosephappiosTests
+//
+//  Created by Julian Carvajal on 3/01/25.
+//
+
+import Testing
+@testable import stjosephappios
+
+struct stjosephappiosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
